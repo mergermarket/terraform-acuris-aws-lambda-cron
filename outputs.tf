262f85ec1eb82aef6a61b60d18dba9055d50cc8d
Keir Badger <keir.badger@acuris.com>
2019-04-30 13:31:33 +0100

    Revert outputs back

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "lambda_arn" {
-  value = "${module.lambda.lambda_arn}"
+  value = "${aws_lambda_function.lambda_function.arn}"
 }
 
 output "lambda_iam_role_name" {
-  value = "${module.lambda.lambda_iam_role_name}"
+  value = "${aws_iam_role.iam_for_lambda.name}"
 }
