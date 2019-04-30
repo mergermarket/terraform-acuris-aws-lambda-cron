674f672fa18647bd5cd7569f4034b91f3cac5889
Keir Badger <keir.badger@acuris.com>
2019-04-30 13:12:15 +0100

    Revert back to not using base module

--- main.tf
+++ main.tf
@@ -1,17 +1,22 @@
-module "lambda" {
-  source                       = "mergermarket/aws-lambda/acuris"
-  version                      = "0.0.3"
-  s3_bucket                    = "${var.s3_bucket}"
-  s3_key                       = "${var.s3_key}"
-  function_name                = "${var.function_name}"
-  handler                      = "${var.handler}"
-  runtime                      = "${var.runtime}"
-  timeout                      = "${var.timeout}"
-  datadog_log_subscription_arn = "${var.datadog_log_subscription_arn}"
-  memory_size                  = "${var.memory_size}"
-  lambda_env                   = "${var.lambda_env}"
-  subnet_ids                   = "${var.subnet_ids}"
-  security_group_ids           = "${var.security_group_ids}"
+resource "aws_lambda_function" "lambda_function" {
+  s3_bucket     = "${var.s3_bucket}"
+  s3_key        = "${var.s3_key}"
+  function_name = "${var.function_name}"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "${var.handler}"
+  runtime       = "${var.runtime}"
+  timeout       = "${var.timeout}"
+  memory_size   = "${var.memory_size}"
+  layers        = ["${var.layer}"]
+
+  vpc_config {
+    subnet_ids         = ["${var.subnet_ids}"]
+    security_group_ids = ["${var.security_group_ids}"]
+  }
+
+  environment {
+    variables = "${var.lambda_env}"
+  }
 }
 
 resource "aws_cloudwatch_event_rule" "cron_schedule" {
@@ -23,13 +28,29 @@ resource "aws_cloudwatch_event_rule" "cron_schedule" {
 
 resource "aws_cloudwatch_event_target" "event_target" {
   rule = "${aws_cloudwatch_event_rule.cron_schedule.name}"
-  arn  = "${module.lambda.lambda_arn}"
+  arn  = "${aws_lambda_function.lambda_function.arn}"
+}
+
+resource "aws_cloudwatch_log_group" "lambda_loggroup" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = 7
+  depends_on      = ["aws_lambda_function.lambda_function"]
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${module.lambda.lambda_function_name}"
+  function_name = "${aws_lambda_function.lambda_function.function_name}"
   principal     = "events.amazonaws.com"
   source_arn    = "${aws_cloudwatch_event_rule.cron_schedule.arn}"
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "kinesis_log_stream" {
+  count           = "${var.datadog_log_subscription_arn != "" ? 1 : 0}"
+  name            = "kinesis-log-stream-${var.function_name}"
+  destination_arn = "${var.datadog_log_subscription_arn}"
+  log_group_name  = "${aws_cloudwatch_log_group.lambda_loggroup.name}"
+  distribution    = "ByLogStream"
+  filter_pattern  = ""
+  depends_on      = ["aws_lambda_function.lambda_function"]
 }
