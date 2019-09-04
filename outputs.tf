ba02665264c30ce503f47c5fd306ac91bd7baf21
David Wickes <david.wickes@acuris.com>
2019-09-04 14:40:42 +0000

    expose iam_role arn

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "lambda_arn" {
 output "lambda_iam_role_name" {
   value = "${aws_iam_role.iam_for_lambda.name}"
 }
+
+output "lambda_iam_role_arn" {
+  value = "${aws_iam_role.iam_for_lambda.arn}"
+}
